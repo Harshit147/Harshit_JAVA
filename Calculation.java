e7e57fc1563f6c6872112b025c05767a35dab7c4
Harshit <harshitparashar345@gmail.com>
2020-08-28 11:25:08 +0530

    Initial Commit

---
+++ Calculation.java
@@ -0,0 +1,16 @@
+class Calculation{
+    int z;
+
+    public void addition(int x, int y) {
+
+    }
+
+    int z= int x + int y;
+    System.out.println("addition is :"+z);
+
+}
+    public static void main(Strings args[]){
+    int x= 10,int y =20;
+    new_calc = Calculations();
+    new_calc.addition(int x , int y);
+    }
